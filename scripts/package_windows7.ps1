088711af4067adb560e5d239f6e174dca2c0a20b
zl <1402686787@qq.com>
2026-07-15 17:37:01 +0800

    Revert "fix: build Windows 7 package without pip upgrade"
    This reverts commit 01836dfd104321889359c727f8a4a6fec0cb7c29.

--- scripts/package_windows7.ps1
+++ scripts/package_windows7.ps1
@@ -8,6 +8,8 @@ Set-Location $RootDir
 
 $VenvDir = Join-Path $RootDir '.venv-win7'
 $VenvPython = Join-Path $VenvDir 'Scripts\python.exe'
+$VenvPip = Join-Path $VenvDir 'Scripts\pip.exe'
+$PyInstaller = Join-Path $VenvDir 'Scripts\pyinstaller.exe'
 
 if (-not (Test-Path $VenvPython)) {
     if ($env:PYTHON38) {
@@ -19,11 +21,12 @@ if (-not (Test-Path $VenvPython)) {
 
 $env:PYQTGRAPH_QT_LIB = 'PySide2'
 $env:RTT_QT_BINDING = 'PySide2'
-& $VenvPython -m pip install -r requirements-win7.txt
+& $VenvPip install -U 'pip<25'
+& $VenvPip install -r requirements-win7.txt
 
 if (Test-Path build-win7) { Remove-Item -Recurse -Force build-win7 }
 if (Test-Path dist-win7) { Remove-Item -Recurse -Force dist-win7 }
-& $VenvPython -m PyInstaller rtt_t2.spec --noconfirm --workpath build-win7 --distpath dist-win7
+& $PyInstaller rtt_t2.spec --noconfirm --workpath build-win7 --distpath dist-win7
 
 if (-not $Version) { $Version = 'v1.0.7' }
 $Archive = Join-Path $RootDir "dist-win7\rtt_t2-$Version-windows7-x64.zip"
